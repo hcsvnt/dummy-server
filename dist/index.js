8374701c5a1b19ea6f949870f66b65830d5e42ac
Mateusz Hallala <matt@huncwot.com>
2024-02-20 00:05:03 +0100

    add: a super simple server with two routes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
-import { helper } from './utils/helper.js';
-console.log(helper);
+// build a simple server
+import express from 'express';
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+// reply with html
+app.get('/hello', (req, res) => {
+    const name = req.query.name || 'World';
+    res.send(`<h1>Hello ${name}</h1>`);
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 //# sourceMappingURL=index.js.map
